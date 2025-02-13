e04c3f60be8f16cb0222cbfb5620a80458ff7870
pogi <153417737+eljohn132@users.noreply.github.com>
2025-02-13 22:35:25 +0800

    app.js

---
+++ app.js
@@ -0,0 +1,22 @@
+{
+    "mapConfig": {
+        "defaultCenter": {
+            "lat": -33.8688,
+            "lng": 151.2195
+        },
+        "defaultZoom": 13
+    },
+    "apiConfig": {
+        "googleMapsApiKey": "AlzaSynfhKupET1aB_1XFAmyYEhorIItN_i1oSa",
+        "libraries": ["geometry", "places"]
+    },
+    "searchBox": {
+        "placeholder": "Search for a place",
+        "width": "300px",
+        "fontSize": "14px"
+    },
+    "features": {
+        "autocomplete": true,
+        "placeMarkerOnSelect": true
+    }
+}
