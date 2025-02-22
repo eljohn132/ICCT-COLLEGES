3adc8115cf87505e243b1804db5b73f136c4cd0f
pogi <153417737+eljohn132@users.noreply.github.com>
2025-02-22 15:54:06 +0800

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 "mapConfig": {
         "defaultCenter": {
-            "lat": -14.61776, // Default center coordinates (Sydney, Australia)
+            "lat": -14.61776, // Default center coordinates (Cainta,Icct colleges
+                )
             "lng": 121.10 257
         },
         "defaultZoom": 13 // Default zoom level
